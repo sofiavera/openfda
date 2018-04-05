3e544434bd93c46603bb633301525947493b1462
s.vera.2017 <veracarretero@gmail.com>
2018-04-05 11:58:35 +0200

    Module IV. HTTP basics practice

---
+++ HTTP basics practice/server_web.py
@@ -0,0 +1,48 @@
+# A basic web server using sockets
+
+
+import socket
+
+PORT = 8092
+MAX_OPEN_REQUESTS = 5
+path = "hola.html"
+path1 ="hugo.html"
+f = open(path,"r")
+g =open(path1,"r")
+
+def process_client(clientsocket):
+    print(clientsocket)
+    message = str(clientsocket.recv(1024))
+    if "new" in message:
+	    web_contents = g.read()
+    else: 
+	    web_contents = f.read()
+    web_headers = "HTTP/1.1 200"
+    web_headers += "\n" + "Content-Type: text/html"
+    web_headers += "\n" + "Content-Length: %i" % len(str.encode(web_contents))
+    clientsocket.send(str.encode(web_headers + "\n\n" + web_contents))
+    clientsocket.close()
+
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+hostname = socket.gethostname()
+# Let's use better the local interface name
+hostname = "localhost"
+try:
+    serversocket.bind((hostname, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print ("Waiting for connections at %s %i" % (hostname, PORT))
+        (clientsocket, address) = serversocket.accept()
+        # now do something with the clientsocket
+        # in this case, we'll pretend this is a non threaded server
+        process_client(clientsocket)
+
+except socket.error:
+    print("Problemas using port %i. Do you have permission?" % PORT)
