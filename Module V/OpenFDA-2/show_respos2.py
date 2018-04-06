de63137e7ff93e1908364ca927796c61a11b727b
s.vera.2017 <veracarretero@gmail.com>
2018-04-06 11:17:30 +0200

    Done.

---
+++ Module V/OpenFDA-2/show_respos2.py
@@ -0,0 +1,13 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+repos = json.loads(repos_raw)
+print("The name if the manufacturer is", repos['results'][0]['openfda']['manufacturer_name'])
