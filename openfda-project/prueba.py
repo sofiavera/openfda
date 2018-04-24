df5346b1e4f4c3a67544156da9dff8c4ba894ac4
s.vera.2017 <spamdesofia@gmail.com>
2018-04-24 16:44:12 +0200

    Proving.

--- openfda-project/prueba.py
+++ openfda-project/prueba.py
@@ -12,28 +12,23 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
         if self.path == "/":
             with open("search.html", "r") as f:
                 message = f.read()
                 self.wfile.write(bytes(message, "utf8"))
-
-        elif "search" in self.path:
-            headers = {'User-Agent': 'http-client'}
-            conn = http.client.HTTPSConnection("api.fda.gov")
-            data = self.path.strip("search?").split("&")
-            drug = data[0].split("=")[1]
-            limit = data[1].split("=")[1]
-            url = "/drug/label.json?search=active_ingredient:" + drug + "&" + "limit=" + limit
-            print(url)
-            conn.request("GET", url, None, headers)
+        elif self.path == "searchDrug":
+            list_url = self.path.strip("searchDrug")
+            active_ingredient = list_url[1]
+            final_url = "/drug/label.json?search=active_ingredient:" + active_ingredient
+            conn.request("GET", final_url, None, headers)
             r1 = conn.getresponse()
+            print(r1.status, r1.reason)
             drugs_raw = r1.read().decode("utf-8")
             conn.close()
             drugs = json.loads(drugs_raw)
             new_drugs = str(drugs)
             self.wfile.write(bytes(new_drugs, "utf8"))
-        return
+
 
 
 Handler = testHTTPRequestHandler
