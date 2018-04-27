dbd37cb69ed126b2c564709c2fa1613e3e02e8f7
s.vera.2017 <spamdesofia@gmail.com>
2018-04-27 17:01:28 +0200

    AAAAAAHHH QUE YA VAAAAAA :') SIIIIINCOOOOOO JEJEJEJE

--- openfda-project/prueba.py
+++ openfda-project/prueba.py
@@ -113,7 +113,6 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             with open('empty.html', 'r') as f:
                 file = f.read()
                 self.wfile.write(bytes(file, "utf8"))
-                print("aaag")
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
