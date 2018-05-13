163a865d170a9753deea8e2d93a65c6d3dc7d455
s.vera.2017 <spamdesofia@gmail.com>
2018-05-13 14:19:33 +0200

    ???

--- openfda-project/proof.py
+++ openfda-project/proof.py
@@ -5,18 +5,23 @@ import json
 
 IP = 'localhost'
 PORT = 8000
-with open("search.html", "r") as f:
-    message = f.read()
-    self.wfile.write(bytes(message, "utf8"))
-class OpenFDAClient(self, first_param, limit):
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    input = self.path.split("=")
-    first_param = input[0]
-    limit = input[1]
-    def get_url (first_param,limit):
+
+
+class OpenFDAClient(self):
+    def get_url(first_param, limit):
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        input = self.path.split("=")
+        first_param = input[0]
+        limit = input[1]
         url = "/drug/label.json?search=active_ingredient:" + first_param + "=" + limit
         conn.request("GET", url, None, headers)
         r1 = conn.getresponse()
         drugs_raw = r1.read().decode("utf-8")
         conn.close()
-get_url()
+
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    with open("search.html", "r") as f:
+        message = f.read()
+        self.wfile.write(bytes(message, "utf8"))
+first_step = OpenFDAClient
+first_step.get_url()
